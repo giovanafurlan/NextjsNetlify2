f752af30272e0a0415861ca3e68ef878fea1108e
giovanafurlan <giovananelofurlan@gmail.com>
2022-06-20 14:48:25 -0300

    Novo tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -11,12 +11,12 @@ export default function Document() {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KCFGBKW'); 
+        })(window,document,'script','dataLayer','GTM-WNQWRRJ');
         `}}/>
       </Head>
       <body>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCFGBKW"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNQWRRJ"
         height="0" width="0"></iframe></noscript>
         <Main />
         <NextScript />
